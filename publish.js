3002d75a40f495737e15d6b471d533751ce5e09d
Berend <berend.swennenhuis@trimm.nl>
2021-01-16 23:13:32 +0100

    dist

--- publish.js
+++ publish.js
@@ -2,5 +2,5 @@ const ghpages = require('gh-pages');
 ghpages.publish('dist', {
   repo: "https://github.com/Berenddeperend/tetris.git"
 }, function(err) {
-  console.log('ddint wurk', err)
+  console.log('uploaded succesful.')
 });

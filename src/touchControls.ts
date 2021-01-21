a8c1f9dd7aad001cb7e47d29eac4d1a24b50c89e
Berend <berend.swennenhuis@trimm.nl>
2021-01-21 22:19:17 +0100

    proof of concept: touchcontrols

--- src/touchControls.ts
+++ src/touchControls.ts
@@ -3,27 +3,38 @@ import Stage from "./stage";
 export default class TouchControls {
   stage: Stage;
   onTap: (e: any) => any;
+  onTapRelease: (e: any) => any;
   deviceWidth: number;
+  // action : () => void;
+  interval: number;
+
 
   constructor(stage: Stage) {
     this.deviceWidth = document.querySelector('body').clientWidth;
     this.stage = stage;
 
     this.onTap = (e:TouchEvent) => {
-      console.log('e.touches[0].clientX: ', e.touches[0].clientX);
-
-      e.touches[0].clientX > this.deviceWidth / 2
+      const action = ()=> {
+        e.touches[0].clientX > this.deviceWidth / 2
         ? stage.controls.right()
         : stage.controls.left()
+      }
+
+      action();
+      this.interval = window.setInterval(action, 100);
     };
+ 
+    this.onTapRelease = (e:TouchEvent) => {
+      clearInterval(this.interval);
+    }
 
     this.init();
   }
 
   init() {
     console.log("initialized touchcontrols");
-    // document.addEventListener("click", this.onTap);
     document.addEventListener("touchstart", this.onTap);
+    document.addEventListener("touchend", this.onTapRelease);
   }
 
   destroy() {
